b8142b28b02ea4d5d279cfd986e6017a6acd5f8a
Fernando <fstanchese@gmail.com>
2022-05-10 11:18:35 -0300

    ajustes

--- src/main/java/br/com/integra/api/security/config/CorsConfig.java
+++ src/main/java/br/com/integra/api/security/config/CorsConfig.java
@@ -1,29 +1,34 @@
 package br.com.integra.api.security.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+
 @Configuration
 public class CorsConfig {
 
+	@SuppressWarnings("rawtypes")
 	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
+    public FilterRegistrationBean customCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
         config.addAllowedOrigin("*");
-        config.addAllowedHeader("*"); 
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-	}
+        @SuppressWarnings("unchecked")
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+
+        //IMPORTANT #2: I didn't stress enough the importance of this line in my original answer,
+        //but it's here where we tell Spring to load this filter at the right point in the chain
+        //(with an order of precedence higher than oauth2's filters)
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 }
